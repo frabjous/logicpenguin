a546584dbf07f33c038dacf3528e3728a651c121
Kevin Klement <klement@umass.edu>
2023-06-28 15:27:05 -0400

    kron push 2023-06-28 15:27:05

--- app/makeequivdb.js
+++ app/makeequivdb.js
@@ -2,3 +2,30 @@ import appsettings from './lpsettings.js';
 import lpfs from './lpfs.js';
 import { loadEquivalents } from '../public/js/symbolic/libequivalence-db.js';
 
+const datadir = appsettings.datadir;
+const assignmentdir = datadir + '/kck/34254/exercises';
+
+let go = async function() {
+    const jsonfiles = await lpfs.filesin(assignmentdir);
+
+    for (let jsonfile of jsonfiles) {
+        let parts = jsonfile.split('-info');
+        if (parts.length < 2) {
+            continue;
+        }
+        let probleminfo = lpfs.loadjson(assignmentdir + '/' + jsonfile);
+        if (!probleminfo) {
+            console.error('Could not load json in ' + jsonfile);
+            process.exit(1);
+        }
+        if (!("problemsets" in probleminfo)) {
+            console.error('No problem sets in ' + jsonfile);
+            process.exit(1);
+        }
+        for (let pset of probleminfo.problemsets) {
+            console.log(pset.problemtype);
+        }
+    }
+}
+
+go();
