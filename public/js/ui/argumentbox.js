c3e53cfa0ce155ec0c14b4732ac7423345b17c7e
Kevin Klement <klement@umass.edu>
2023-07-05 16:27:07 -0400

    kron push 2023-07-05 16:27:07

--- public/js/ui/argumentbox.js
+++ public/js/ui/argumentbox.js
@@ -2,18 +2,22 @@
 // Public License along with this program. If not, see
 // https://www.gnu.org/licenses/.
 
+///////////////////////////////////////////////////////////////////////
+// creates a little table for a natural language argument            //
+///////////////////////////////////////////////////////////////////////
+
 import { addelem } from '../common.js';
 
 export function argumentBox(prems, conc) {
-        let box = document.createElement("table");
+        const box = document.createElement("table");
         box.classList.add("nlargument");
         // argument box
-        let tbody = addelem('tbody', box, {});
-        for (let prem of prems) {
-            let tr = addelem('tr', tbody, {});
-            let td = addelem('td', tr, { innerHTML: prem });
+        const tbody = addelem('tbody', box, {});
+        for (const prem of prems) {
+            const tr = addelem('tr', tbody, {});
+            const td = addelem('td', tr, { innerHTML: prem });
         }
-        let ctr = addelem('tr', tbody, {});
-        let ctd = addelem('td', ctr, { innerHTML: '∴ ' + conc });
+        const ctr = addelem('tr', tbody, {});
+        const ctd = addelem('td', ctr, { innerHTML: '∴ ' + conc });
         return box;
 }
