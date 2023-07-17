cd089b341a7e78e213b79eb77a19c5f355aa74f9
Kevin Klement <klement@umass.edu>
2023-07-17 12:47:07 -0400

    kron push 2023-07-17 12:47:07

--- public/js/checkers/rules/hardegree-rules.js
+++ public/js/checkers/rules/hardegree-rules.js
@@ -6,13 +6,17 @@
 // defines the inference rules of Hardegree's system                     //
 ///////////////////////////////////////////////////////////////////////////
 
-export default {
+import notations from '../../symbolic/notations.js';
+
+const opnames = ['NOT','OR','AND','IFF','IFTHEN','FORALL','EXISTS','FALSUM'];
+
+const hardegreeRules = {
     "→I"  : { meinongian: true, hint: "To establish a →-statement, use CD in a subderivation for it, even if you have to write in a SHOW-line yourself." },
     "∨I"  : { forms: [ { prems: ["A"], conc: "A ∨ B" }, { prems: ["A"], conc: "B ∨ A" } ] },
     "&I"  : { forms: [ { prems: ["A", "B"], conc: "A & B" } ] },
     "↔I"  : { forms: [ { prems: ["A → B", "B → A"], conc: "A ↔ B" } ] },
     "✖I"  : { forms: [ { prems: ["A", "~A"], conc: "✖" } ] },
-    "∀I"  : { pred: true, meinongian: true, hint: "To establish a ∀-statement, use CD in a subderivation for it, even if you have to write in a SHOW-line yourself." },
+    "∀I"  : { pred: true, meinongian: true, hint: "To establish a ∀-statement, use UD in a subderivation for it, even if you have to write in a SHOW-line yourself." },
     "∃I"  : { pred: true, forms: [ { prems: ["Aa"], conc: "∃xAx", subst: {"x":"a"} } ] },
     "Ass" : { assumptionrule: true },
     "→O"  : { forms: [ { prems: ["A → C", "A"], conc: "C" }, { prems: ["A → C", "~C"], conc: "~A" } ] },
@@ -50,4 +54,55 @@ export default {
     "~✖I" : { hidden: true, meinongian: true, hint: "Although in-rules would be nice, only out-rules exist for negations." },
     "~∀I" : { pred: true, hidden: true, meinongian: true, hint: "Although in-rules would be nice, only out-rules exist for negations." },
     "~∃I" : { pred: true, hidden: true, meinongian: true, hint: "Although in-rules would be nice, only out-rules exist for negations." }
+}
+
+function substituteSymbols(s, notationname) {
+    const innotation = notations["hardegree"];
+    const outnotation = notations[notationname];
+    for (const op of opnames) {
+        s = s.replaceAll(innotation[op], outnotation[op]);
+    }
+    return s;
+}
+
+export default function getHardegreeRuleset(notationname) {
+    // don't bother if we are just returning the name
+    if (notationname == 'hardegree') { return hardegreeRules; }
+    // bind change function to new notation
+    const change = ((s) => (substituteSymbols(s, notationname)));
+    // start a new rule set and populate with changed rules
+    const ruleset = {};
+    for (const rulename in hardegreeRules) {
+        const newrulename = change(rulename);
+        const rule = hardegreeRules[rulename];
+        if ("hint" in rule) {
+            rule.hint = change(rule.hint);
+        }
+        if ("forms" in rule) {
+            for (const form of rule.forms) {
+                if ("conc" in form) {
+                    form.conc = change(form.conc);
+                }
+                if ("prems" in form) {
+                    for (let i=0; i<form.prems.length; i++) {
+                        form.prems[i] = change(form.prems[i]);
+                    }
+                }
+                if ("subderivs" in form) {
+                    for (const subderiv of form.subderivs) {
+                        if ("needs" in subderiv) {
+                            for (let i=0; i<subderiv.needs.length; i++) {
+                                subderiv.needs[i] = change(subderiv.needs[i]);
+                            }
+                        }
+                        if ("allows" in subderiv) {
+                            subderiv.allows = change(subderiv.allows);
+                        }
+                    }
+                }
+            }
+        }
+        ruleset[newrulename] = rule;
+    }
+    return ruleset;
 }
