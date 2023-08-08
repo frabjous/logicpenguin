faa9cbdfd681683343d4bbb0202803e2ac73a9f0
Kevin Klement <klement@umass.edu>
2023-08-08 11:07:06 -0400

    kron push 2023-08-08 11:07:06

---
+++ public/js/instructor.js
@@ -0,0 +1,11 @@
+// LICENSE: GNU GPL v3 You should have received a copy of the GNU General
+// Public License along with this program. If not, see
+// https://www.gnu.org/licenses/.
+
+//////////////////// instructor.js //////////////////////////////////////
+// defines the main functions controlling the instructor page          //
+/////////////////////////////////////////////////////////////////////////
+
+const LPinstr = {};
+
+export default LPinstr;
