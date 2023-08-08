e543ac07587f13fdf55588f5c9762cf1ddfe2287
Kevin Klement <klement@umass.edu>
2023-08-08 11:47:06 -0400

    kron push 2023-08-08 11:47:06

--- public/js/instructor.js
+++ public/js/instructor.js
@@ -6,6 +6,40 @@
 // defines the main functions controlling the instructor page          //
 /////////////////////////////////////////////////////////////////////////
 
+import LP from '../load.js';
+
+// initialize stuff
 const LPinstr = {};
+const mainAreasLoaded = {};
+
+// convenience constants
+const byid = LP.byid;
+const msgArea = byid('messagearea');
+
+
+function clearMessage() {
+    msgArea.display.style = 'none';
+    msgArea.classList.remove('info', 'loading', 'warning', 'error');
+    msgArea.innerHTML = '';
+}
+
+function infoMessage(msg) {
+    this.makeMessage('info', msg);
+}
+
+function loadhash(h) {
+    LP.infoMessage('Well, hello there.');
+}
+
+function makeMessage(msgtype, msg) {
+    msgArea.display.style = 'block';
+    msgArea.classList.remove('info','loading','warning','error');
+    msgArea.classList.add(msgtype);
+    msgArea.innerHTML = msg;
+}
+
+// start stuff
+
 
 export default LPinstr;
+
