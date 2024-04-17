86e882ef29bb61867bfc93497b09b61d7c0dc01c
Kevin Klement <klement@umass.edu>
2024-04-17 16:09:27 -0400

    kron push 2024-04-17 16:09:27

--- public/js/creators/symbolic-translation.js
+++ public/js/creators/symbolic-translation.js
@@ -2,7 +2,7 @@
 // Public License along with this program. If not, see
 // https://www.gnu.org/licenses/.
 
-/////////////////// creators/valid-correct-sound.js /////////////////////
+/////////////////// creators/symbolic-translation.js ////////////////////
 // class for creating problems for translating natural language        //
 // statements into symbolic notation                                   //
 /////////////////////////////////////////////////////////////////////////
@@ -124,7 +124,13 @@ export default class TranslationExerciseCreator extends LogicPenguinProblemSetCr
             if (this.answerer) {
                 continue;
             }
-            this.answerer = addelem('div', this.ansinfoarea);
+            this.makeAnswerer('');
+            this.mypsc.makeChanged();
+        };
+        pc.makeAnswerer = function(ans) {
+            this.answerer = addelem('div', this.ansinfoarea, {
+                mypc: this
+            });
             this.answered.label = addelem('div', this.answerer, {
                 innerHTML: tr('Translation') + ':'
             });
@@ -133,97 +139,28 @@ export default class TranslationExerciseCreator extends LogicPenguinProblemSetCr
                 pred: (this?.mypsc?.predradio?.checked),
                 lazy: (!!(this?.mypsc?.predradio?.checked))
             });
+            this.answerer.fmlinput.value = ans;
+            this.answerer.fmlinput.mypc = this;
+            this.answered.fmlinput.onchange = function() {
+                this?.mypc?.mypsc.makeChanged();
+            }
+            this.answered.fmlinput.oninput = function() {
+                this?.mypc?.mypsc.makeChanged();
+            }
+            // once answers are possible, can no longer change type
             if (this.mypsc.disableRadios) { this.mypsc.disableRadios(); }
-
+        }
+        if (answer && answer != '') {
+            pc.makeAnswerer(answer);
         }
         pc.getProblem = function() {
             return this.totransinput.value;
         }
-
-        pc.mip = multiInputField(pc.probinfoarea, 'Premise', problem?.prems ?? [], 2);
-        pc.mip.mypc = pc;
-        pc.mip.onchange = function() { this.mypc.whenchanged(); }
-        pc.mip.oninput = function() { this.mypc.whenchanged(); }
-        const concdiv = addelem('div', pc.probinfoarea, {
-            classes: ['fielddiv']
-        });
-        const conclabel = addelem('div', concdiv, {
-            innerHTML: tr('Conclusion')
-        });
-        pc.concinput = addelem('input', concdiv, {
-            value: (problem?.conc ?? ''),
-            mypc: pc,
-            oninput: function() { this.mypc.whenchanged(); },
-            onchange: function() { this.mypc.whenchanged(); }
-        });
-        pc.origanswer = answer;
-        pc.origproblem = problem;
-        pc.mypsc = this;
-        pc.makeAnswerer = function() {
-            let ans = this.origanswer;
-            const prob = this.getProblem();
-            if (this?.answerer) {
-                ans = this.getAnswer();
-                const a = this.answerer;
-                a.parentNode.removeChild(a);
-            }
-            if (!sufficesForProblem(prob)) { return; }
-            this.answerer = addelem('valid-correct-sound', this.ansinfoarea);
-            this.answerer.makeProblem(prob, this.mypsc.gatherOptions() , 'save');
-            this.answerer.processAnswer = function() {};
-            this.answerer.mypc = this;
-            this.answerer.makeChanged = function() {
-                const newans = this.getAnswer();
-                this.restoreAnswer(serverAnswerToUserAnswer(newans));
-                this.mypc.mypsc.makeChanged();
-            };
-            this.answerer.restoreAnswer(ans);
-        }
-        pc.whenchanged = function() {
-            const nowprob = this.getProblem();
-            if (sufficesForProblem(nowprob)) {
-                this.ansbelowlabel.style.display = 'block';
-                if ("lastmade" in this && sameProblem(this.lastmade, nowprob)) {
-                    return;
-                }
-                this.lastmade = nowprob;
-                this.ansinfoarea.innerHTML = '';
-                this.makeAnswerer();
-            } else {
-                if (this?.ansbelowlabel) {
-                    this.ansbelowlabel.style.display = 'none';
-                }
-                if (this?.ansinfoarea) {
-                    this.ansinfoarea.innerHTML = '';
-                }
-            }
-        }
-        pc.getProblem = function() {
-            return {
-                conc: this.concinput.value,
-                prems: this.mip.getvalues()
-            }
-        }
         pc.getAnswer = function() {
-            if (this.answerer) {
-                const userans = this.answerer.getAnswer();
-                delete(userans.sound);
-                return userans;
-            }
-            return {
-                valid: -2,
-                correct: -2
-            };
+            if (!(this.answerer)) { return ''; }
+            return this.answerer.fmlinput.value;
         }
-        if (!isnew) {
-            if ("conc" in problem) {
-                pc.concinput.value = problem.conc;
-            }
-            pc.whenchanged();
-            if (pc.answerer) {
-                pc.answerer.restoreAnswer(serverAnswerToUserAnswer(answer));
-            }
-        }
+
         return pc;
     }
 
@@ -237,6 +174,6 @@ export default class TranslationExerciseCreator extends LogicPenguinProblemSetCr
 
 }
 
-customElements.define("valid-correct-sound-creator", ValidCorrectSoundCreator);
+customElements.define("symbolic-translation-creator", TranslationExerciseCreator);
 
 
