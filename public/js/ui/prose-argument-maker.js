4f46f0d739e577fccb88dfbf9c3a357d98218e45
Kevin Klement <klement@umass.edu>
2024-04-19 15:24:53 -0400

    kron push 2024-04-19 15:24:53

--- public/js/ui/prose-argument-maker.js
+++ public/js/ui/prose-argument-maker.js
@@ -29,8 +29,14 @@ function randomid() {
     return rv;
 }
 
+function triggerChange() {
+    if (this?.myargdiv?.whenchanged) {
+        this.myargdiv.whenchanged();
+    }
+}
+
 export function getProseArgumentMaker(parNode,
-    argdetails = [], translations = [], conc = -1, opts = {}) {
+    argdetails = [], translations = [], concnum = -1, opts = {}) {
     // insert a blank statement if no argument already
     if (argdetails.length == 0) {
         argdetails.push({ statement: '' });
@@ -42,9 +48,10 @@ export function getProseArgumentMaker(parNode,
         id: newPAMid()
     });
 
-    argDiv.addStatement = function(deets, trans, checked) = function() {
+    argDiv.addStatement = function(deets, trans, concchecked) = function() {
         const block = addelem('div', this, {
-            classes: ['statementblock']
+            classes: ['statementblock'],
+            id: randomid()
         });
         const tbl = addelem('table', block);
         const tbdy = addelem('tbody', tbl);
@@ -54,32 +61,41 @@ export function getProseArgumentMaker(parNode,
         const preltd = addelem('td', pretr);
         const statementltd = addelem('td', statementtr);
         const postltd = addelem('td', posttr);
-        const prelbl = addelem('span', preltd, {
-            innerHTML: tr('preceding text')
+        const prelbl = addelem('label', preltd, {
+            innerHTML: tr('preceding text'),
+            htmlFor: block.id + '-preinp'
         });
-        const statementlbl = addelem('span', statementltd, {
-            innerHTML: tr('statement')
+        const statementlbl = addelem('label', statementltd, {
+            innerHTML: tr('statement'),
+            htmlFor: block.id + '-statementinp'
         });
-        const postlbl = addelem('span', postltd, {
-            innerHTML: tr('following text')
+        const postlbl = addelem('label', postltd, {
+            innerHTML: tr('following text'),
+            htmlFor: block.id + '-postinp'
         });
         const preitd = addelem('td', pretr);
         const statementitd = addelem('td', statementtr);
         const postitd = addelem('td', posttr);
         block.preinp = addelem('input', preitd, {
-            type: 'text'
+            type: 'text',
+            id: block.id + '-preinp',
+            myargdiv: this
         });
         if ("pre" in deets) {
             block.preinp.value = deets.pre;
         };
         block.statementinp = addelem('input', statementitd, {
-            type: 'text'
+            type: 'text',
+            id: block.id + '-statementinp',
+            myargdiv: this
         });
         if ("statement" in deets) {
             block.statementinp.value = deets.statement;
         }
         block.postinp = addelem('input', postitd, {
-            type: 'text'
+            type: 'text',
+            id: block.id + '-postinp',
+            myargdiv: this
         });
         if ("post" in deets) {
             block.postinp.value = deets.post;
@@ -87,50 +103,43 @@ export function getProseArgumentMaker(parNode,
         if (("gettrans" in this.opts) && this.opts.gettrans) {
             const transtr = addelem('tr', tbdy);
             const transltd = addelem('td', transtr);
-            const translbl = addelem('span', transltd, {
-                innerHTML: tr('translation')
+            const translbl = addelem('label', transltd, {
+                innerHTML: tr('translation'),
+                htmlFor: block.id + '-transinp'
             });
             const transitd = addelem('td', transtr);
             block.transinp = FormulaInput.getnew(this.opts);
             transitd.appendChild(block.transinp);
+            block.transinp.id = block.id + '-transinp';
+            block.transinp.myargdiv = this;
         }
         const radiodiv = addelem('div', block);
         block.concradio = addelem('input', radiodiv, {
-
-        })
-
-    }
-    // subdiv for each statement
-    for (let sdetails of argdetails) {
-        // space between statements
-        if (needTN) {
-            const tN = document.createTextNode(" ");
-            argDiv.appendChild(tN);
+            type: 'radio',
+            name: this.id + 'radios',
+            id: block.id + '-concinp',
+            checked: concchecked,
+            myargdiv: this
+        });
+        block.conclabel = addelem('label', radiodiv, {
+            innerHTML: tr('this is the conclusion'),
+            htmlFor: block.id + '-concinp'
+        });
+        const inps = [block.concradio, block.preinp,
+            block.statement,inp, block.postinp];
+        if ("transinp" in block) {
+            inps.push(block.transinp);
         }
-        needTN = true;
-        // text before main statement
-        if (sdetails.pre) {
-            addelem('span', argDiv, {
-                innerHTML: htmlEscape(sdetails.pre),
-                classes: ['proseargpretext']
-            });
-        }
-        // statement itself
-        if (sdetails.statement) {
-            argDiv.statementSpans.push(
-                addelem('span', argDiv, {
-                    innerHTML: htmlEscape(sdetails.statement),
-                    classes: ['proseargstatement']
-                })
-            );
-        }
-        // text after main statement
-        if (sdetails.post) {
-            addelem('span', argDiv, {
-                innerHTML: htmlEscape(sdetails.post),
-                classes: ['proseargposttext']
-            });
+        for (const inp of inps) {
+            inp.onchange = triggerChange;
+            inp.oninput = triggerChange;
         }
     }
+    for (let i = 0; i < argdetails.length ; i++) {
+        const deets = argdetails[i];
+        const trans = translations?.[i] ?? '';
+        argDiv.addStatement(deets, trans, (i == concnum));
+    }
+
     return argDiv;
 }
